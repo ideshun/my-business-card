d0a5855fcd1fd6ed136ad735a39abf11072aae92
liudeshun <admin@w3h5.com>
2021-04-22 14:50:46 +0800

    Initial commit

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
+		<u-cell-group>
+			<u-cell-item icon="setting-fill" title="个人设置"></u-cell-item>
+			<u-cell-item icon="integral-fill" title="会员等级" value="新版本"></u-cell-item>
+		</u-cell-group>
 	</view>
 </template>
 
@@ -11,7 +11,6 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
 			}
 		},
 		onLoad() {
@@ -24,26 +23,5 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
